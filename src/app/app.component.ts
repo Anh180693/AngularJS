6a342ee3e8ece3ddabe63ef79c61353c8f1a9db7
Anh180693 <anhltth2203042@fpt.edu.vn>
2022-11-19 11:43:30 +0700

    KiemTra

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import profile from 'data.json'
+import { Profile } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exam1';
+  datainfo:Profile|undefined;
+  data=[
+    {
+      name: "Margaret",
+      title: "Sales Respresentative",
+      date: "10/5/1996",
+      address: "7 Houndtood Rd",
+      city: "London",
+      Phone: "(71) 555-4444",
+      img: "assets/img/human2.png",
+    },{
+      name: "Albert",
+      title: "Sales Respresentative",
+      date: "10/5/1996",
+      address: "7 Houndtood Rd",
+      city: "London",
+      Phone: "(71) 555-4444",
+      img: "assets/img/human3.png",
+    },
+    {
+      name: "Michale",
+      title: "Sales Respresentative",
+      date: "10/5/1996",
+      address: "7 Houndtood Rd",
+      city: "London",
+      Phone: "(71) 555-4444",
+      img: "assets/img/human4.png",
+    }
+  ]
+  constructor(){
+    this.datainfo=this.data[0];
+  }
+
+  onClick(index:any){
+    this.datainfo=this.data[index-1];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
